6fe61781caa789fb0a5f78ac345bbe4680ac7e42
Tarun Kumar <srfashions.tarun@gmail.com>
2025-03-18 19:01:54 +0530

    manually add yaml

--- testdriver/test-search-functionality.yml.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.pubnub.com/');
-  await page.getByRole('button').filter({ hasText: 'CtrlK' }).click();
-  await page.getByPlaceholder('Search or ask anything').click();
-  await page.getByPlaceholder('Search or ask anything').fill('real-time interactive apps\n');
-});
