43e897d1d192b2330a94ee2896ee23128c4a49e9
Tarun Kumar <srfashions.tarun@gmail.com>
2025-03-18 21:08:36 +0530

    remove existing playwright tests

--- testdriver/test-call-to-action-buttons.yml.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.pubnub.com/');
-  await page.getByRole('link', { name: 'Start Building' }).click();
-  await page.goto('https://admin.pubnub.com/register');
-});
--- testdriver/test-cookie-consent.yml.spec.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.pubnub.com/');
-  await page.getByRole('button', { name: 'Accept Cookies' }).click();
-});
